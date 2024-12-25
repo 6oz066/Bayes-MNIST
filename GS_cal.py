69e535d37be402e07ee222ed4a3368aa60e2f5dc
szcwq <akayue05@gmail.com>
2024-12-25 22:16:18 +0800

    Primary

--- GS_cal.py
+++ GS_cal.py
@@ -9,6 +9,9 @@ def gaussian_2d(x, y, xi, yi, sigma, alpha):
 def mse(e1, e2):
     return np.mean((e1 - e2) ** 2)
 
+def squared_error(e1, e2):
+    return (e1 - e2) ** 2
+
 def add_gaussians(A, N, T):
     # A is the picture, N is number and T is squared error
     rows, cols = A.shape
@@ -29,14 +32,45 @@ def add_gaussians(A, N, T):
         if result.success:
             xi, yi, sigma, alpha = result.x
             G = gaussian_2d(X,Y,xi,yi,sigma,alpha)
-            B += G
+            # Ignore the contribution out of 3*T
             current_mse = mse(A, B)
+            sqr = squared_error(A, B)
+            distances = np.sqrt((X - xi) ** 2 + (Y - yi) ** 2)
+            # G[distances > 3 * sqr] = 0
+            B+=G
             mse_values.append(current_mse)
-            print(f"Iteration {i + 1}: MSE = {current_mse:.6f}")
             if current_mse < T:
                 print(f"Stopping early at iteration {i + 1}, MSE = {current_mse:.4f}")
                 break
-    return B, mse_values
+    return B, mse_values, initial_params
+
+
+def addGaussians_oo(A, N, T,params):
+    # A is the picture, N is number and T is squared error
+    rows, cols = A.shape
+    B = np.zeros_like(A)
+    mse_values = []
+    X, Y = np.meshgrid(np.arange(cols), np.arange(rows))
+    for i in range(N):
+        def loss(params):
+            xi, yi, sigma, alpha = params
+            G = gaussian_2d(X, Y, xi, yi, sigma, alpha)
+            return mse(A, B + G)
+        result = minimize(loss, params, bounds=[(0, cols), (0, rows), (3, 20), (0.01, 1)])
+        if result.success:
+            xi, yi, sigma, alpha = result.x
+            G = gaussian_2d(X, Y, xi, yi, sigma, alpha)
+            # Ignore the contribution out of 3*T
+            current_mse = mse(A, B)
+            sqr = squared_error(A, B)
+            distances = np.sqrt((X - xi) ** 2 + (Y - yi) ** 2)
+            # G[distances > 3 * sqr] = 0
+            B += G
+            mse_values.append(current_mse)
+            if current_mse < T:
+                print(f"Stopping early at iteration {i + 1}, MSE = {current_mse:.4f}")
+                break
+    return B, mse_values, params
 
 def picture_deal(img,N,T):
     R_channel = img[:, :, 0] / 255.0
@@ -49,10 +83,27 @@ def picture_deal(img,N,T):
     A = grayscale / grayscale.max()
     N_values = N
     T /= 255
-    B, mse_values = add_gaussians(A, N_values, T)
+    B, mse_values,params = add_gaussians(A, N_values, T)
     reconstructed_image = np.stack((B * (R_channel / A), B * (G_channel / A), B * (B_channel / A)), axis=-1)
     reconstructed_image = np.clip(reconstructed_image, 0, 1)
-    return reconstructed_image,mse_values
+    return reconstructed_image,mse_values,params
+
+def picture_deal_oo(img,N,T,params):
+    R_channel = img[:, :, 0] / 255.0
+    G_channel = img[:, :, 1] / 255.0
+    B_channel = img[:, :, 2] / 255.0
+    if img.ndim == 3:
+        grayscale = rgb2gray(img)
+    else:
+        grayscale = img / 255.0
+    A = grayscale / grayscale.max()
+    N_values = N
+    T /= 255
+    B, mse_values, params_new = addGaussians_oo(A, N_values, T,params)
+    reconstructed_image = np.stack((B * (R_channel / A), B * (G_channel / A), B * (B_channel / A)), axis=-1)
+    reconstructed_image = np.clip(reconstructed_image, 0, 1)
+    return reconstructed_image, mse_values, params_new
+
 
 def showmse(mse_values):
     plt.plot(range(1, len(mse_values) + 1), mse_values, marker='o')
--- Video.py
+++ Video.py
@@ -1,6 +1,37 @@
+import time
 import cv2
+import GS_cal as gs
+from tqdm import tqdm
 
-video = cv2.VideoCapture(0)
+# Set the initial value
+N=20
+T=5
+
+# Capturing the video cap and dealing
+cv2.destroyAllWindows()
+input_video = cv2.VideoCapture('sample-mp4-file.mp4')
+fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # For MP4 videos
+print(fourcc)
+output_video = cv2.VideoWriter('output.mp4',fourcc, 20.0, (640,480))
+
 cnt = 0 # Counter for reading the first cap
-while True:
-    ret, frame = video.read()
+for i in tqdm(range(22),'process'):
+    ret, frame = input_video.read()
+    if ret == True:
+        # If it is the first cap
+        if cnt == 0:
+            cv2.imshow('frame',frame)
+            new_frame,msevalue,params= gs.picture_deal(frame,N, T)
+            output_video.write(new_frame)
+            cnt += 1
+            gs.showmse(msevalue)
+        # For the other cap
+        else:
+            new_frame,msevalue,params= gs.picture_deal_oo(frame,N, T,params)
+            output_video.write(new_frame)
+            # cv2.imshow('frame',new_frame)
+
+# Release the resource
+input_video.release()
+output_video.release()
+cv2.destroyAllWindows()
